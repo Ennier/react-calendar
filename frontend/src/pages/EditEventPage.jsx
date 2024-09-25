8693a3da50951ce14968f672213d82b42fd78dea
Ennier Manuel Navea Castillo <enavea90@gmail.com>
2024-09-25 22:39:31 +0200

    create app pages for routing

---
+++ frontend/src/pages/EditEventPage.jsx
@@ -0,0 +1,9 @@
+function EditEventPage(params) {
+    return (
+        <h1>
+            Edit Event Page
+        </h1>
+    );
+}
+
+export default EditEventPage;
---
+++ frontend/src/pages/EventsPage.jsx
@@ -0,0 +1,9 @@
+function EventsPage(params) {
+    return (
+        <h1>
+            Events Page
+        </h1>
+    );
+}
+
+export default EventsPage;
